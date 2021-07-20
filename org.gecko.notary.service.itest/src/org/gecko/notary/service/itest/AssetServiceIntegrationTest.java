45fa8b187d3c549cd85ae48bb550a79396643935
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2021-07-20 15:26:58 +0200

    License EPL 2.0
    - cleanup
    - switched licenses
    
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.notary.service.itest/src/org/gecko/notary/service/itest/AssetServiceIntegrationTest.java
+++ org.gecko.notary.service.itest/src/org/gecko/notary/service/itest/AssetServiceIntegrationTest.java
@@ -15,17 +15,6 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-/**
- * Copyright (c) 2012 - 2018 Data In Motion and others.
- * All rights reserved. 
- * 
- * This program and the accompanying materials are made available under the terms of the 
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- * 
- * Contributors:
- *     Data In Motion - initial API and implementation
- */
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
