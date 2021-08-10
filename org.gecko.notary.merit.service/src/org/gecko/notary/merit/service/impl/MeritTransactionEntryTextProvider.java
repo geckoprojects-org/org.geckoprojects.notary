16498fd2653b16aaae8c4dee9b7ba1c6b2e44be4
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2021-08-10 15:29:30 +0200

    - remove duplicates
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.notary.merit.service/src/org/gecko/notary/merit/service/impl/MeritTransactionEntryTextProvider.java
+++ org.gecko.notary.merit.service/src/org/gecko/notary/merit/service/impl/MeritTransactionEntryTextProvider.java
@@ -11,30 +11,21 @@
  */
 package org.gecko.notary.merit.service.impl;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.gecko.notary.merit.model.merit.AchievementEntry;
 import org.gecko.notary.merit.model.merit.BetResultType;
+import org.gecko.notary.merit.model.merit.BettingEntry;
 import org.gecko.notary.merit.model.merit.MeritPackage;
 import org.gecko.notary.merit.model.merit.PurchaseEntry;
-import org.gecko.notary.merit.model.merit.BettingEntry;
-import org.gecko.notary.merit.model.merit.AchievementEntry;
-import org.gecko.notary.model.notary.NotaryFactory;
-import org.gecko.notary.model.notary.NotaryPackage;
-import org.gecko.notary.model.notary.Participant;
-import org.gecko.notary.model.notary.Transaction;
-import org.gecko.notary.model.notary.TransactionEntry;
 import org.gecko.notary.model.notary.AssetTransactionEntry;
+import org.gecko.notary.model.notary.NotaryPackage;
+import org.gecko.notary.model.notary.TransactionEntry;
 import org.gecko.notary.service.api.ParticipantService;
 import org.gecko.notary.service.api.TransactionService;
+import org.gecko.notary.service.api.textprovider.AbstractTransactionEntryTextProvider;
 import org.gecko.notary.service.api.textprovider.TextProvider;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -44,11 +35,9 @@ import org.osgi.service.component.annotations.Reference;
  * @author Mark Hoffmann
  * @since 23.07.2021
  */
-@Component(property = {"object=TransactionEntry", "target=TransactionEntry", "domain=Merit", "service.rank=100"})
-public class MeritTransactionEntryTextProvider implements TextProvider {
+@Component(service = TextProvider.class, property = {"object=TransactionEntry", "target=TransactionEntry", "domain=Merit", "service.rank=100"})
+public class MeritTransactionEntryTextProvider extends AbstractTransactionEntryTextProvider {
 
-	private static final Logger logger = Logger.getLogger(MeritTransactionEntryTextProvider.class.getName());
-	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
 	public static final String PURCHASE_LABEL = "[%s] An amount of %s merits has been purchased from provider %s with transaction id: '%s'";
 	public static final String PLACE_BET_LABEL = "[%s] A bet was placed with a stake of %s merits for bet '%s'";
 	public static final String BET_RESULT_LABEL = "[%s] The bet '%s' was finished with a result %s and a stake of %s merits";
@@ -63,55 +52,27 @@ public class MeritTransactionEntryTextProvider implements TextProvider {
 	
 	/* 
 	 * (non-Javadoc)
-	 * @see org.gecko.notary.service.api.textprovider.TextProvider#provideText(org.eclipse.emf.ecore.EObject, java.util.Map)
+	 * @see org.gecko.notary.service.api.textprovider.AbstractTransactionEntryTextProvider#getParticipantService()
 	 */
 	@Override
-	public String provideText(EObject object, Map<String, Object> properties) {
-		if (!(object instanceof TransactionEntry)) {
-			logger.log(Level.WARNING, "Object is not of instance TransactionEntry. Returning without result");
-			return null;
-		}
-		TransactionEntry te = (TransactionEntry) object;
-		String transactionId = te.getTransactionId();
-		// Set transaction information
-		Transaction t = transactionService.getTransactionById(transactionId);
-		if (t == null) {
-			if (te instanceof AssetTransactionEntry) {
-				t = NotaryFactory.eINSTANCE.createTransaction();
-				t.setDescription(ASSET_DEFAULT_LABEL);
-			} else {
-				logger.log(Level.WARNING, ()->String.format("[%s] No transaction found. Returning without result", transactionId));
-				return null;
-			}
-		}
-		te.setLabel(t.getDescription());
-		// Set source information
-		Participant p = participantService.getParticipant(te.getParticipantId());
-		if (p == null) {
-			logger.log(Level.WARNING, ()->String.format("[%s] No participant found. Cannot set source information", te.getParticipantId()));
-		} else {
-			String source = p.getDescription();
-			if (source == null) {
-				source = p.getName();
-			}
-			te.setSource(source);
-		}
-		String comment = createComment(te);
-		if (comment != null) {
-			te.setComment(comment);
-			return comment;
-		}
-		return null;
+	protected ParticipantService getParticipantService() {
+		return participantService;
 	}
 	
-	/**
-	 * Creates a comment for a certain {@link TransactionEntry}
-	 * @param entry the 
-	 * @param transaction
-	 * @param participant
-	 * @return
+	/* 
+	 * (non-Javadoc)
+	 * @see org.gecko.notary.service.api.textprovider.AbstractTransactionEntryTextProvider#getTransactionService()
 	 */
-	private String createComment(TransactionEntry entry) {
+	@Override
+	protected TransactionService getTransactionService() {
+		return transactionService;
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.gecko.notary.service.api.textprovider.AbstractTransactionEntryTextProvider#doCreateComment(org.gecko.notary.model.notary.TransactionEntry)
+	 */
+	protected String doCreateComment(TransactionEntry entry) {
 		List<EStructuralFeature> features = new LinkedList<>();
 		String text = null;
 		if (entry instanceof PurchaseEntry) {
@@ -139,73 +100,9 @@ public class MeritTransactionEntryTextProvider implements TextProvider {
 			features.add(MeritPackage.Literals.ACHIEVEMENT_ENTRY__MERIT_AMOUNT);
 			text = ACHIEVEMTENT_RESULT_LABEL;
 		} else if (entry instanceof AssetTransactionEntry) {
-			AssetTransactionEntry ate = (AssetTransactionEntry) entry;
-			features.add(NotaryPackage.Literals.TRANSACTION_ENTRY__ASSET_ID);
-			features.add(NotaryPackage.Literals.ASSET_TRANSACTION_ENTRY__ASSET);
-			switch (ate.getChangeType()) {
-			case CREATION:
-				text = CREATION_TEMLPATE;
-				break;
-			case DESTRUCTION:
-				text = DESTRUCTION_TEMLPATE;
-				break;
-			case JOIN:
-				features.add(NotaryPackage.Literals.ASSET_TRANSACTION_ENTRY__PARENT_ASSET_ID);
-				text = JOIN_TEMPLATE;
-				break;
-			case MODIFICATION:
-				text = MODIFICATON_TEMPLATE;
-				break;
-			case OWNERSHIP:
-				features.add(NotaryPackage.Literals.ASSET_TRANSACTION_ENTRY__CHANGE_DATA);
-				features.add(NotaryPackage.Literals.TRANSACTION_ENTRY__PARTICIPANT_ID);
-				text = OWNERSHIP_TEMPLATE;
-				break;
-			case SPLIT:
-				features.add(NotaryPackage.Literals.ASSET_TRANSACTION_ENTRY__PARENT_ASSET_ID);
-				text = SPLIT_TEMPLATE;
-				break;
-			default:
-				break;
-			}
+			return super.createComment(entry);
 		}
 		return text == null ? null : getText(text, entry, features);
 	}
 	
-	/**
-	 * Returns the formatted text from the given template string, using the entry parameter as object
-	 * and the list of {@link EStructuralFeature} as provider for the string formatting parameters.
-	 * @param template the string template 
-	 * @param entry the {@link EObject}, to get values from
-	 * @param features the features to get value for
-	 * @return the formatted {@link String}
-	 */
-	private String getText(String template, TransactionEntry entry, List<EStructuralFeature> features) {
-		if (template == null || entry == null) {
-			return null;
-		}
-		if (features == null || features.isEmpty()) {
-			return template;
-		}
-		Object[] values = features.stream()
-				.map(entry::eGet)
-				.map(this::mapToString)
-				.collect(Collectors.toList())
-				.toArray();
-		return String.format(template, values);
-	}
-	
-	private String mapToString(Object object) {
-		if (object == null) {
-			return "<n/a>";
-		}
-		if (object instanceof Date) {
-			return sdf.format(object);
-		} else if (object instanceof EObject) {
-			return ((EObject)object).eClass().getName();
-		} else {
-			return object.toString();
-		}
-	}
-
 }
--- org.gecko.notary.service/src/org/gecko/notary/service/impl/textprovider/TransactionEntryTextProvider.java
+++ org.gecko.notary.service/src/org/gecko/notary/service/impl/textprovider/TransactionEntryTextProvider.java
@@ -11,26 +11,10 @@
  */
 package org.gecko.notary.service.impl.textprovider;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
-
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.gecko.notary.model.notary.AssetTransactionEntry;
-import org.gecko.notary.model.notary.NotaryFactory;
-import org.gecko.notary.model.notary.NotaryPackage;
-import org.gecko.notary.model.notary.ParentAssetEntry;
-import org.gecko.notary.model.notary.Participant;
-import org.gecko.notary.model.notary.Transaction;
 import org.gecko.notary.model.notary.TransactionEntry;
 import org.gecko.notary.service.api.ParticipantService;
 import org.gecko.notary.service.api.TransactionService;
+import org.gecko.notary.service.api.textprovider.AbstractTransactionEntryTextProvider;
 import org.gecko.notary.service.api.textprovider.TextProvider;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -40,11 +24,9 @@ import org.osgi.service.component.annotations.Reference;
  * @author Mark Hoffmann
  * @since 19.03.2020
  */
-@Component(property = {"object=TransactionEntry", "target=TransactionEntry"})
-public class TransactionEntryTextProvider implements TextProvider {
+@Component(service = TextProvider.class, property = {"object=TransactionEntry", "target=TransactionEntry"})
+public class TransactionEntryTextProvider extends AbstractTransactionEntryTextProvider {
 
-	private static final Logger logger = Logger.getLogger(TransactionEntryTextProvider.class.getName());
-	private final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
 	@Reference
 	private TransactionService transactionService;
 	@Reference
@@ -52,125 +34,27 @@ public class TransactionEntryTextProvider implements TextProvider {
 
 	/* 
 	 * (non-Javadoc)
-	 * @see de.dim.diamant.service.api.TextProvider#provideText(org.eclipse.emf.ecore.EObject, java.util.Map)
+	 * @see org.gecko.notary.service.api.textprovider.AbstractTransactionEntryTextProvider#getParticipantService()
+	 */
+	protected ParticipantService getParticipantService() {
+		return participantService;
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.gecko.notary.service.api.textprovider.AbstractTransactionEntryTextProvider#getTransactionService()
+	 */
+	protected TransactionService getTransactionService() {
+		return transactionService;
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.gecko.notary.service.api.textprovider.AbstractTransactionEntryTextProvider#doCreateComment(org.gecko.notary.model.notary.TransactionEntry)
 	 */
 	@Override
-	public String provideText(EObject object, Map<String, Object> properties) {
-		if (!(object instanceof TransactionEntry)) {
-			logger.log(Level.WARNING, "Object is not of instance TransactionEntry. Returning without result");
-			return null;
-		}
-		TransactionEntry te = (TransactionEntry) object;
-		String transactionId = te.getTransactionId();
-		// Set transaction information
-		Transaction t = transactionService.getTransactionById(transactionId);
-		if (t == null) {
-			if (te instanceof AssetTransactionEntry) {
-				t = NotaryFactory.eINSTANCE.createTransaction();
-				t.setDescription(ASSET_DEFAULT_LABEL);
-			} else {
-				logger.log(Level.WARNING, ()->String.format("[%s] No transaction found. Returning without result", transactionId));
-				return null;
-			}
-		}
-		te.setLabel(t.getDescription());
-		// Set source information
-		Participant p = participantService.getParticipant(te.getParticipantId());
-		if (p == null) {
-			logger.log(Level.WARNING, ()->String.format("[%s] No participant found. Cannot set source information", te.getParticipantId()));
-		} else {
-			String source = p.getDescription();
-			if (source == null) {
-				source = p.getName();
-			}
-			te.setSource(source);
-		}
-		String comment = createComment(te);
-		if (comment != null)
-			te.setComment(comment);
-		return comment;
-	}
-
-	/**
-	 * Creates a comment for a certain {@link TransactionEntry}
-	 * @param entry the 
-	 * @return
-	 */
-	private String createComment(TransactionEntry entry) {
-		List<EStructuralFeature> features = new LinkedList<>();
-		String text = null;
-		if (entry instanceof ParentAssetEntry) {
-			features.add(NotaryPackage.Literals.PARENT_ASSET_ENTRY__PARENT_ASSET_ID);
-			features.add(NotaryPackage.Literals.TRANSACTION_ENTRY__ASSET_ID);
-			text = PARENT_TEMLPATE;
-		} else if (entry instanceof AssetTransactionEntry) {
-			AssetTransactionEntry ate = (AssetTransactionEntry) entry;
-			features.add(NotaryPackage.Literals.TRANSACTION_ENTRY__ASSET_ID);
-			features.add(NotaryPackage.Literals.ASSET_TRANSACTION_ENTRY__ASSET);
-			switch (ate.getChangeType()) {
-			case CREATION:
-				text = CREATION_TEMLPATE;
-				break;
-			case DESTRUCTION:
-				text = DESTRUCTION_TEMLPATE;
-				break;
-			case JOIN:
-				features.add(NotaryPackage.Literals.ASSET_TRANSACTION_ENTRY__PARENT_ASSET_ID);
-				text = JOIN_TEMPLATE;
-				break;
-			case MODIFICATION:
-				text = MODIFICATON_TEMPLATE;
-				break;
-			case OWNERSHIP:
-				features.add(NotaryPackage.Literals.ASSET_TRANSACTION_ENTRY__CHANGE_DATA);
-				features.add(NotaryPackage.Literals.TRANSACTION_ENTRY__PARTICIPANT_ID);
-				text = OWNERSHIP_TEMPLATE;
-				break;
-			case SPLIT:
-				features.add(NotaryPackage.Literals.ASSET_TRANSACTION_ENTRY__PARENT_ASSET_ID);
-				text = SPLIT_TEMPLATE;
-				break;
-			default:
-				break;
-			}
-		}
-		return text == null ? null : getText(text, entry, features);
+	protected String doCreateComment(TransactionEntry entry) {
+		return null;
 	}
 	
-	/**
-	 * Returns the formatted text from the given template string, using the entry parameter as object
-	 * and the list of {@link EStructuralFeature} as provider for the string formatting parameters.
-	 * @param template the string template 
-	 * @param entry the {@link EObject}, to get values from
-	 * @param features the features to get value for
-	 * @return the formatted {@link String}
-	 */
-	private String getText(String template, TransactionEntry entry, List<EStructuralFeature> features) {
-		if (template == null || entry == null) {
-			return null;
-		}
-		if (features == null || features.isEmpty()) {
-			return template;
-		}
-		Object[] values = features.stream()
-				.map(entry::eGet)
-				.map(this::mapToString)
-				.collect(Collectors.toList())
-				.toArray();
-		return String.format(template, values);
-	}
-	
-	private String mapToString(Object object) {
-		if (object == null) {
-			return "<n/a>";
-		}
-		if (object instanceof Date) {
-			return sdf.format(object);
-		} else if (object instanceof EObject) {
-			return ((EObject)object).eClass().getName();
-		} else {
-			return object.toString();
-		}
-	}
-
 }
