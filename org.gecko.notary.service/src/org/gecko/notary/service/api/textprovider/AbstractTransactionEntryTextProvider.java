df094083ffa59031a20e3aff9c26cd71a9e5889d
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2021-08-10 16:03:58 +0200

    License check
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.notary.service/src/org/gecko/notary/service/api/textprovider/AbstractTransactionEntryTextProvider.java
+++ org.gecko.notary.service/src/org/gecko/notary/service/api/textprovider/AbstractTransactionEntryTextProvider.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012 - 2021 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available under the terms of the 
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
 package org.gecko.notary.service.api.textprovider;
 
 import java.text.SimpleDateFormat;
